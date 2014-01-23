9a8cb2012fa18f0b61088e929ad253dcbee0e57e
shilixiao <shilixiao@rd.tuan800.com>
2014-01-23 16:20:19 +0800

    解决冲突master与xxx分支冲突

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,49 +12,26 @@
 require 'spec_helper'
 
 describe User do
-<<<<<<< HEAD
-  
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
-=======
-
   before do
     @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar" )
   end
 
->>>>>>> modeling-users2
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
-<<<<<<< HEAD
-
-  it { should be_valid }
-=======
-  it { should respond_to(:password_digest) }
+ it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
->>>>>>> modeling-users2
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
   end
-<<<<<<< HEAD
-
-  describe "when name is too long" do
-    before { @user.name = "a"* 51 }
-    it { should_not be_valid }
-  end
-
-  describe "when email is not present" do
-    before { @user.email = " " }
-    it { should_not be_valid}
-  end
-=======
-  
+ 
   describe "when name is too long" do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
@@ -134,5 +111,4 @@ describe User do
     end
   end
 
->>>>>>> modeling-users2
 end
